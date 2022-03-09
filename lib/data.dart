877e9ef74dbbe0de615a7df04cf36bf832a84403
emojivibe <93862405+emojivibe@users.noreply.github.com>
2022-03-09 23:21:57 +0530

    Create data.dart

---
+++ lib/data.dart
@@ -0,0 +1,12 @@
+Map<String, String> dataMap = {
+  "question1":
+      "RIDDLE: i speak without a mouth and hear without a ear,i have no body,but i come alive with wind. what am i?",
+  "hint": "you do this on a mountain cliff",
+  "question2":
+  "riddle:You measure my life in hours and I serve you by expiring. I’m quick when I’m thin and slow when I’m fat. The wind is my enemy.",
+  "hint2":"you use to look through it when it gets dark",
+  "Image": "echo.webp",
+  "Image2": "yaay.jpg",
+  "Img":"candle.jpg",
+  "img2":"d4.jpg"
+};
